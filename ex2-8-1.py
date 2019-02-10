383908a2d8f0b9451557dfa891cd614f60e1b9f9
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-10 01:47:20 +0100

    Add more exercises from Ch2 from the portal

---
+++ ex2-8-1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 01:33:10 2019
+
+@author: Ravi
+"""
+"""
+2-8.1
+
+Oefenopgave 1
+
+Wat is het verschil tussen de volgende vier uitdrukkingen? (Probeer in de IDLE-shell.)
+1) 17.31 + 16.69
+2) '17.31 ' + ' 16.69'
+3) int(17.31) + int(16.69),
+4) int(float('17.31')) + int('16.69')
+ 
+Wat zijn de verschillen tussen int() en round()?
+"""
+
+print('1):1 returns a float, 2 returns a string, 3 returns an int and 4 throws an error.')
+print('2): int() just drops the remainder, round() actually changes the value.' )
+
+
---
+++ ex2-8-2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 01:45:31 2019
+
+@author: Ravi
+"""
+'''
+Oefenopgave 2
+Beantwoord de vragen 1 tot en met 7 van de Quiz aan het einde van hoofdstuk 5 van Learning.
+Beantwoord eerst de vragen en kijk pas later naar de antwoorden.
+'''
