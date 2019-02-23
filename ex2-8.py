607163a5cc010f9ec139384006228c34dcd9483b
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-23 02:10:40 +0100

    update ch 2, ex8

---
+++ ex2-8.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 01:45:31 2019
+
+@author: Ravi
+"""
+'''
+Oefenopgave 1
+
+Wat is het verschil tussen de volgende vier uitdrukkingen? (Probeer in de IDLE-shell.)
+1) 17.31 + 16.69
+2) '17.31 ' + ' 16.69'
+3) int(17.31) + int(16.69),
+4) int(float('17.31')) + int('16.69')
+
+Oefenopgave 2
+Beantwoord de vragen 1 tot en met 7 van de Quiz aan het einde van hoofdstuk 5 van Learning.
+Beantwoord eerst de vragen en kijk pas later naar de antwoorden.
+'''
+
+print("1): ")
+print("1 geeft een float, 2 geeft een string, 3 geeft een int en 4 geeft een error. Onduidelijk waaorm.")
+
+print("2): done.")
+
+'''
+Oefenopgave 3
+Beantwoord de vragen 2.1 tot en met 2.6 aan het einde van hoofdstuk 2 in Introducing. Ook hier geldt: eerst beantwoorden, later controleren.
+'''
+
+print("3): ")
+
