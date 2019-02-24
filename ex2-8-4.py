7f15cb524a33ad2b7ea76118a653c7559b296694
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-24 01:21:26 +0100

    Complete 2-8 ex4

---
+++ ex2-8-4.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 01:17:43 2019
+
+@author: Ravi
+
+# 4 Oefenen met rekenkundige expressies
+getal1 = 25
+getal2 = 3
+# Pas de operators toe in de tabel onder 
+# "Numbers" in "Py Ingredients" van "Introducing Python" 
+# op getal1 en getal2
+""" 
+getal1 = 25
+getal2 = 3
+
+sum = getal1 + getal2 
+sub = getal1 - getal2 # vervang + door aftrekken
+mul = getal1 * getal2 # vervang + door vermenigvuldigen
+fdv = getal1 // getal2 # vervang + door deling zonder rest
+idv = getal1 / getal2 # vervang + door deling met rest
+mod = getal1 % getal2 # vervang + door modulo (geeft de rest)
+exp = getal1 ** getal2 # vervang + door machtsverheffen
+
+print(sum,sub, mul, fdv, idv, mod, exp) 
+
