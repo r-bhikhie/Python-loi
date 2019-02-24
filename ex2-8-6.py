1020cec0aaa0044ac4428067748d70e493052138
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-24 01:35:24 +0100

    Complete 2-8 ex 6 and chapter 2

---
+++ ex2-8-6.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 01:28:43 2019
+
+@author: Ravi
+
+In het programma treft u stukjes tekst aan toegekend aan variabelen.
+Maak van die tekst getallen en tel ze bij elkaar op.
+Druk het product (vermenigvuldig de drie getallen met elkaar) af.
+"""
+
+# 6 Converteer tekst naar getal
+# Converteer de tekst-variablen (t1,t2,t3)
+# naar de getallen (g1,g2,g3)
+# en bereken het product van die getallen
+t1 = "117"
+t2 = "19.3"
+t3 = "-36.0"
+# vul de toekenningen aan
+g1 = int(t1)
+g2 = float(t2)
+g3 = float(t3)
+product = g1*g2*g3
+print(product)
