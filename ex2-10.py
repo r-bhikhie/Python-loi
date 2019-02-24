6f70c4871ccaa00a91f530ade8d04a5cef514a3c
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-24 01:52:29 +0100

    Complete 2-8 ex 10

---
+++ ex2-10.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 01:41:06 2019
+
+@author: Ravi
+Oefenopgave 1
+Ga naar het programma 7. Apostrofs en aanhalingstekens in tekst.
+
+Druk onderstaande twee strings af:
+
+baby's flesje
+Mao's speech: "Duizend bloemen zullen bloeien ...."
+
+Oefenopgave 2
+Voer de instructies uit in het programma 8. Apostrofs en aanhalingstekens in tekst (2).
+"""
+
+print("Ex 1:")
+print("baby's flesje")
+
+print("Ex 2:")
+print('''Mao's speech: "Duizend bloemen\nzullen bloeien ...."''')
+
+'''
+# ==============================================
+# 8 Apostrofs en aanhalingstekens in tekst (2)
+# ==============================================
+# Druk het onderstaande tekstfragment af. 
+# Omring de string met single quotes en 
+# gebruik escape sequences.
+#
+"""
+Een aantal python functies:
+    str(type) => string
+    float('string'_or_number) => float
+    int('string'_or_number)  => int
+""" 
+# Hieronder uw uitwerking
+'''
+
+print("Een aantal python functies:")
+print("\tstr(type) => string")
+print("\tfloat(\'string\'_or_number) => float")
+print("\tint(\'string\'_or_number)  => int")
