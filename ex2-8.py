892a7f207c6e4af50c22b4f0b595f6cc85570d36
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-02-24 01:15:25 +0100

    Complete 2-8 ex3

--- ex2-8.py
+++ ex2-8.py
@@ -12,15 +12,15 @@ Wat is het verschil tussen de volgende vier uitdrukkingen? (Probeer in de IDLE-s
 2) '17.31 ' + ' 16.69'
 3) int(17.31) + int(16.69),
 4) int(float('17.31')) + int('16.69')
+'''
+print("1): ")
+print("1 geeft een float, 2 geeft een string, 3 geeft een int en 4 geeft een error. Onduidelijk waaorm.")
 
+'''
 Oefenopgave 2
 Beantwoord de vragen 1 tot en met 7 van de Quiz aan het einde van hoofdstuk 5 van Learning.
 Beantwoord eerst de vragen en kijk pas later naar de antwoorden.
 '''
-
-print("1): ")
-print("1 geeft een float, 2 geeft een string, 3 geeft een int en 4 geeft een error. Onduidelijk waaorm.")
-
 print("2): done.")
 
 '''
@@ -29,4 +29,14 @@ Beantwoord de vragen 2.1 tot en met 2.6 aan het einde van hoofdstuk 2 in Introdu
 '''
 
 print("3): ")
+print("3)(2.1):", 60*60)
+secs_per_hour = 60*60
+print("3)(2.2): secs_per_hour = 60*60")
+secs_in_day = secs_per_hour*24
+print("3)(2.4 and 2.3): secs_in_day = secs_per_hour*24:", secs_in_day)
+print("3)(2.5): ", secs_in_day/secs_per_hour)
+print("3)(2.5): Yes:", secs_in_day//secs_per_hour)
 
+'''
+
+'''
