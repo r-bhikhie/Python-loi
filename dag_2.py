43ac8d3b3e9cccd7b6fa6999abc0e7a9d8dc2cd1
Ravi Bhikhie <r.bhikhie@gmail.com>
2019-03-16 12:45:34 +0100

    Add paperclip added method

--- dag_2.py
+++ dag_2.py
@@ -1,18 +1,21 @@
 # Run this with python -i to land in the interactive shell.
-#
-#
+
 class Person:
     pop = 0
     def __init__(self, name, yob):
         self.name = name
         self.yob = yob
         Person.pop += 1
+        self.paperclips = 0
     def __del__(self):
         Persoon -= 1
     def __str__(self):
         # iets
         returnstring = "This is a Person object: {0} was born in {1}".format(self.name, self.yob)
         return returnstring
+    def __add__(self, other):
+        self.paperclips += int(other)
+        print("{0} now has {1} paperclips!".format(self.name, self.paperclips))
 
 class Car:
     nr_wheels = 4
@@ -30,4 +33,8 @@ class Morse(Car):
 
 auto1 = Car('OEMlogo')
 morse1 = Morse('duck')
+
 alice = Person('Alice', 1996)
+bob = Person('Bob', 1985)
+eve = Person('Eve', 2000)
+
